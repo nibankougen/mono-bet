ccb390bfb37e2f8d8481f53ad415bf27833b8c8c
KiyosatoSomeya <kiyo_8into32@me.com>
2025-08-12 16:08:55 +0900

    added eslint

---
+++ eslint.config.ts
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig({
+  files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+  plugins: { js },
+  extends: [
+    "js/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended"
+  ],
+  languageOptions: {
+    globals: globals.browser,
+  },
+});
