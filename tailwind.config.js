ed723b0477cbaf0cd3c39669d43b619bfcf4ddaa
KiyosatoSomeya <kiyo_8into32@me.com>
2025-08-14 20:12:02 +0900

    extend tailwind colors

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        ...colors,
+        'orange': '#f7931e',
+        'blue': '#29abe2',
+        'ivory': '#fffbee',
+      }
+    }
+  }
+}
