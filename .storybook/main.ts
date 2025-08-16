1e7298a78359b7cfa4f3b5f1294032c6f01051c9
KiyosatoSomeya <kiyo_8into32@me.com>
2025-08-16 17:55:18 +0900

    install vite-plugin-svgr

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import svgr from "vite-plugin-svgr";
 
 const config: StorybookConfig = {
   "stories": [
@@ -15,6 +16,11 @@ const config: StorybookConfig = {
   "framework": {
     "name": "@storybook/react-vite",
     "options": {}
-  }
+  },
+  "viteFinal": async (config) => {
+    config.plugins = config.plugins || [];
+    config.plugins.push(svgr());
+    return config;
+  },
 };
 export default config;
