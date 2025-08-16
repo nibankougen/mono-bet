b0a8850b65a5d76948aed7d61cbb5d88b55e51cc
KiyosatoSomeya <kiyo_8into32@me.com>
2025-08-16 17:55:57 +0900

    add Icon

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,40 @@
+import StarImage from "./images/Star.svg?react";
+import TriangleImage from "./images/Triangle.svg?react";
+
+export interface IconProps {
+  size: IconSize;
+  color: number;
+  image: number;
+}
+
+type IconSize = 24 | 48;
+
+const SizeTagMap: Record<IconSize, string> = {
+  24: "w-[24px] h-[24px]",
+  48: "w-[48px] h-[48px]",
+};
+
+const ColorTagMap: Record<number, string> = {
+  0: "text-red-500",
+  1: "text-orange-500",
+  2: "text-yellow-500",
+  3: "text-green-500",
+  4: "text-teal-500",
+  5: "text-sky-500",
+  6: "text-blue-500",
+  7: "text-indigo-500",
+  8: "text-purple-500",
+};
+
+export const Icon = ({ size, color, image }: IconProps) => {
+  const className = `${SizeTagMap[size]} ${ColorTagMap[color]} block transition-turn`;
+
+  switch (image) {
+    case 0:
+      return <StarImage className={className} />;
+    case 1:
+      return <TriangleImage className={className} />;
+    default:
+      return <StarImage className={className} />;
+  }
+};
