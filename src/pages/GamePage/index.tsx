2143b2d50b99e97ad0817c6af48177ad533ed8ef
KiyosatoSomeya <kiyo_8into32@me.com>
2025-08-17 18:43:00 +0900

    draft: add GamePage

---
+++ src/pages/GamePage/index.tsx
@@ -0,0 +1,10 @@
+export const GamePage = () => {
+  return (
+    <div className="w-full h-[100dvh] bg-mb-ivory">
+      <div className="w-full overflow-x-auto"></div>
+      <div className="grid grid-cols-[1fr_auto_1fr]">
+        <div></div>
+      </div>
+    </div>
+  );
+}
